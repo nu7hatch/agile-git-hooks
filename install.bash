38e938e307568804ead31a6fb0b4c155454e2c1c
Chris Kowalik <chris@nu7hat.ch>
2012-11-19 17:58:04 -0300

    fixed installation script prompt

--- install.bash
+++ install.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-repo_url="git://github.com/nu7hatch/agile-git-hooks.git"
-temp_dir="/tmp/agile-git-hooks"
-
-exec < /dev/tty
-
 echo "WARNING! This operation may destroy your already configured hooks!"
-read -p "Do you want to proceed the installation? (y/N) " answer
+read -p "Do you want to proceed the installation? (y/N) " answer < /dev/tty
 
 if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
     echo
     echo "Aborting..."
     exit 1
 fi
+
+repo_url="git://github.com/nu7hatch/agile-git-hooks.git"
+temp_dir="/tmp/agile-git-hooks"
 
 if [ -d "$(pwd)/.git" ]; then
     rm -rf $temp_dir
@@ -24,6 +22,6 @@ if [ -d "$(pwd)/.git" ]; then
 
     rm -rf $temp_dir
 else
-    echo "ERROR: Not a git repository."
+    echo "ERROR! Not a git repository."
     exit 1
 fi
